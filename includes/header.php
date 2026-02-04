47d9e2da3d109f09af23f1911306381e8b752bea
duadrien <adrien.dulou@ynov.com>
2026-02-04 10:48:59 +0100

    modif header responsif

--- includes/header.php
+++ includes/header.php
@@ -18,10 +18,12 @@ if (session_status() === PHP_SESSION_NONE) {
                     <li><a href="vente.php">Vendre</a></li>
                     <li><a href="cart.php"><i class="fa-solid fa-cart-shopping"></i> Panier</a></li>
                     <li><a href="account.php">Mon Compte</a></li>
+                    
                     <?php if(isset($_SESSION['role']) && $_SESSION['role'] === 'ADMIN'): ?>
-                        <li><a href="admin/index.php" style="color: var(--gold); border: 1px solid var(--gold); padding: 5px 10px; border-radius: 4px;">ADMIN</a></li>
+                        <li><a href="admin/index.php" class="admin-link">ADMIN</a></li>
                     <?php endif; ?>
-                    <li><a href="logout.php" style="color: #ff6b6b;">Déconnexion</a></li>
+                    
+                    <li><a href="logout.php" class="logout-link">Déconnexion</a></li>
                 
                 <?php else: ?>
                     <li><a href="login.php">Se connecter</a></li>
