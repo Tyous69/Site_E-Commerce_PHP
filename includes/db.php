7a20980a0e85bc61f097df7260a3e8986423eccc
Tyous <dulou.adrien@gmail.com>
2026-02-19 19:35:09 +0100

    informations dans un .env qui est dans un .gitignore et utilisation de variable dans le fichier db.php ducoup

--- includes/db.php
+++ includes/db.php
@@ -1,19 +1,20 @@
 <?php
-// includes/db.php
 
-$host = "localhost";
-$user = "root";
-$password = ""; // Sur Windows/XAMPP, c'est vide par défaut
-$database = "php_exam_db"; // Assure-toi que c'est le nom exact de ta DB dans PhpMyAdmin
+require_once __DIR__ . '/../vendor/autoload.php';
 
-// Connexion à la base de données
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+$database = $_ENV['DB_NAME'];
+
 $mysqli = new mysqli($host, $user, $password, $database);
 
-// Vérification de la connexion
 if ($mysqli->connect_error) {
     die("Erreur de connexion : " . $mysqli->connect_error);
 }
 
-// Forcer l'UTF-8 pour éviter les problèmes d'accents
 $mysqli->set_charset("utf8mb4");
 ?>
